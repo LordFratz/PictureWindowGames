d3ac0337d224efd03e2589683f2746afd094b7b4
exlted <exltedofdrg@gmail.com>
2017-01-07 16:45:45 -0500

    More work (still incomplete) on Render system

--- Picture Window Project/Picture Window Project/Renderer.h
+++ Picture Window Project/Picture Window Project/Renderer.h
@@ -5,20 +5,51 @@
 #include "Common\DeviceResources.h"
 using namespace DirectX;
 
+struct sphere
+{
+	float radius;
+};
+
 class Camera
 {
+public:
 
 };
 
+typedef void(*CleanupFunc)(void);
 typedef void(*RenderFunc)(RenderNode &rNode);
 
 class RenderNode
 {
 	RenderFunc func;
-	RenderNode* next;
 public:
+	RenderNode* next;
+	RenderNode()
+	{
+		next = nullptr;
+	}
+	RenderNode(RenderFunc Func)
+	{
+		next = nullptr;
+		func = Func;
+	}
+	~RenderNode()
+	{
+		delete next;
+	}
 	inline void renderProcess() { func(*this) };
 	inline RenderNode* GetNext() { return next; };
+	inline void AddChild(RenderNode* child)
+	{
+		if(next == nullptr)
+		{
+			next = child;
+		}
+		else
+		{
+			next->AddChild(child);
+		}
+	}
 };
 
 class RenderContext : public RenderNode
@@ -35,6 +66,10 @@ class RenderContext : public RenderNode
 class RenderMesh
 {
 	//TODO: Develop Mesh Storage Here
+	Microsoft::WRL::ComPtr<ID3D11Buffer>		m_indexBuffer;
+	std::vector<void*> MeshData;
+public:
+	CleanupFunc func;
 };
 
 class RenderShape : public RenderNode
@@ -42,12 +77,23 @@ class RenderShape : public RenderNode
 	RenderMesh& Mesh;
 	RenderContext& Context;
 	XMFLOAT4X4 WorldMat;
+	sphere BoundingSphere;
+public:
+	~RenderShape()
+	{
+		Mesh.func();
+		delete next;
+	}
 };
 
 class RenderSet
 {
 	RenderNode* head;
 public:
+	~RenderSet()
+	{
+		delete head;
+	}
 	inline RenderNode* GetHead() { return head; };
 	static RenderSet* CreateSet(Camera& camera);
 };
